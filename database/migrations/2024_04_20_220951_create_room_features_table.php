54fdaffe804af59008892a639d2f71160b5c74a3
Mossab Arektout <mossabarektout2000@gmail.com>
2024-04-21 23:37:00 +0100

    create rooms and features tables

---
+++ database/migrations/2024_04_20_220951_create_room_features_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_features', function (Blueprint $table) {
+            $table->id();
+            $table->text('type');
+            $table->unsignedBigInteger('number_of_rooms');
+            $table->unsignedBigInteger('number_of_beds');
+            $table->boolean('bathroom')->default(false);
+            $table->boolean('balcony')->default(false);
+            $table->boolean('workspace')->default(false);
+            $table->boolean('TV')->default(false);
+            $table->boolean('wifi')->default(false);
+            $table->boolean('minibar')->default(false);      
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_features');
+    }
+};
